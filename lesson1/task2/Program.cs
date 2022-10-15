073acf2ec0121aeb4dc96d776847194819cfef9b
alina12b <alina_bondareva12@mail.ru>
2022-10-15 17:01:33 +0300

    first lesson

---
+++ lesson1/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c) Console.Write($"Большее число a: {a}");
+else if (b > a && b > c) Console.Write($"Большее число b: {b}");
+else if (c > a && c > b) Console.Write($"Большее число c: {c}");
+else if (a > b) Console.Write($"Большие числа a и с: {a}");
+else if (a > c) Console.Write($"Большие числа a и b: {a}");
+else if (c > a) Console.Write($"Большие числа b и с: {b}");
+else Console.Write($"Все числа равны между собой");
---
+++ lesson1/task3/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 1) Console.Write($"Число {a} нечётное");
+else Console.Write($"Число {a} чётное");
