d04ed5541800051f103c6f1223dd20835c02618b
alina12b <alina_bondareva12@mail.ru>
2022-10-22 19:09:20 +0300

    third lesson

---
+++ lesson3/task3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите номер дня недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 1 || a > 7) Console.Write("Дней в неделе всего 7, в следующий раз введите правильно"); 
+else if (a < 6) Console.Write("Это будний день");
+else Console.Write("Ура, это выходной");
