aa69aa1514d022dd7977f0f29528cb52be8dc3f0
alina12b <alina_bondareva12@mail.ru>
2022-12-03 11:17:55 +0300

    sixth lesson

---
+++ lesson6/task1/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] CreateArr() {
+    Console.WriteLine("Введите количество чисел, которые хотите ввести: ");
+    int length = Convert.ToInt32(Console.ReadLine());
+    int[] arr = new int[length];
+    int i = 0;
+
+    Console.WriteLine("Введите же ваши числа: ");
+    while (i < length) {
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+        i++;
+    }
+
+    return arr;
+}
+
+int CountPositiveNums(int[] arr) {
+    int count = 0;
+    foreach (int num in arr) {
+       count += Convert.ToInt32(num > 0);
+    }
+    Console.WriteLine($"Количество положительных чисел среди введенных: {count}");
+    return count;
+}
+
+int[] arr = CreateArr();
+int positives = CountPositiveNums(arr);
---
+++ lesson6/task2/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] CreateLine() {
+    int[] arr = new int[2];
+    Console.Write("k = ");
+    arr[0] = Convert.ToInt32(Console.ReadLine());
+    Console.Write("b = ");
+    arr[1] = Convert.ToInt32(Console.ReadLine());
+
+    return arr;
+}
+
+void FindIntersectionPoint(int[] firstLine, int[] secondLine) {
+    int k1 = firstLine[0];
+    int k2 = secondLine[0];
+    int b1 = firstLine[1];
+    int b2 = secondLine[1];
+    if (k1 == k2 && b1 == b2) Console.WriteLine("Прямые совпадают, точки пересечения везде");
+    else if (k1 == k2) Console.WriteLine("Прямые параллельны, точек пересечения нет");
+    else {
+        double x = (b1 - b2) / (k2 - k1);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Точка пересечения двух прямых: [{x}; {y}]");
+    }
+    return;
+}
+
+Console.WriteLine("Задайте параметры для первой прямой:");
+int[] firstLine = CreateLine();
+
+Console.WriteLine("Задайте параметры для второй прямой:");
+int[] secondLine = CreateLine();
+FindIntersectionPoint(firstLine, secondLine);
